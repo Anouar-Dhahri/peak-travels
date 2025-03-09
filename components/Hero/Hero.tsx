014c6cb13be088877ff3b2145d0abc9c55382d66
Anouar_Dhahri <anouar.dhahri1@gmail.com>
2025-03-09 23:18:44 +0100

    hero-text-content

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,6 +13,19 @@ const Hero = () => {
         preload="metadata"
         className="w-full h-full object-cover"
       />
+      {/** TEXT CONTENT */}
+      <div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="flex items-center justify-center flex-col w-full h-full">
+          <div>
+            <h1 className=" text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold uppercase">
+              Let's Enjoy the nature
+            </h1>
+            <p className="md:text-base text-center text-lg text-white font-normal [word-spacing:5px]">
+              Get the best prices of 2,000,000+ properties,worldwide{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
