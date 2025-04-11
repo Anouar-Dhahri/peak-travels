0d9eaa7b1eb4f53453da50c4493b772bce424caa
Anouar_Dhahri <anouar.dhahri1@gmail.com>
2025-04-11 17:14:28 +0100

    add-aos-animation

--- pages/Home/WhyChoose/WhyChoose.tsx
+++ pages/Home/WhyChoose/WhyChoose.tsx
@@ -10,13 +10,19 @@ const WhyChoose = () => {
         description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis pariatur earum doloremque esse, quidem reprehenderit."
       />
       <div className="grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20">
-        <div>
+        <div data-aos="fade-up" data-aos-anchor-placement="top-center">
           <ChooseCard title="Best Price Guarantee" image="/images/c1.svg" />
         </div>
-        <div>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay={100}>
           <ChooseCard title="Easy & Quick" image="/images/c2.svg" />
         </div>
-        <div>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay={300}>
           <ChooseCard title="Customer Care 24/7" image="/images/c3.svg" />
         </div>
       </div>
